ea73c3c1f55d0e654e49029039cec303b2e50ad2
William Chalk <william@printedproducts.com>
2022-10-21 16:50:12 -0400

    added talk sales button function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+const talkSalesBtn = document.querySelector("#talk-sales");
+
+talkSalesBtn.addEventListener("click",()=>{
+    document.location.href = "./sales.html";
+});
